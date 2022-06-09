2045bc942a24c0892e014fb17518c2cfde75fe3c
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-06-09 14:50:58 -0600

    Add files via upload
    added Exercise13_11 and Octagon classes so I can work on them

---
+++ Chapter 13/Exercise13_11.java
@@ -0,0 +1,10 @@
+package exercise1311;
+
+public class Exercise13_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chapter 13/Octagon.java
@@ -0,0 +1,5 @@
+package exercise1311;
+
+public class Octagon {
+
+}
