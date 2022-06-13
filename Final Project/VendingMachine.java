076df1f69d72eba668426fa61d78d6ec48c40031
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-06-13 14:55:47 -0600

    Add files via upload
    added vendingmachine class

---
+++ Final Project/VendingMachine.java
@@ -0,0 +1,6 @@
+package finalprojectoo;
+
+public class VendingMachine {
+	// remember to change array to ArrayList so it can be be emptied
+	Drink[] options = {new Powerade(), new Powerade(), new Powerade()};
+}
