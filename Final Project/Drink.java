2230e7af90cd9bd703e55979c6e9d8ae930ef664
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-06-13 14:54:36 -0600

    Add files via upload
    Added Drink Interface

---
+++ Final Project/Drink.java
@@ -0,0 +1,9 @@
+package finalprojectoo;
+
+public interface Drink {
+			public String name(); 
+			public String color(); 
+			public String container(); 
+			public String description(); 
+			public String drink(); 
+}
