ade3940c5e3dda28acca00c78c521dcc975dbe55
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-06-13 14:55:16 -0600

    Add files via upload
    added powerade class

---
+++ Final Project/Powerade.java
@@ -0,0 +1,31 @@
+package finalprojectoo;
+
+public class Powerade implements Drink{
+
+	@Override
+	public String color() {
+		return "Blue";
+	}
+
+	@Override
+	public String container() {
+		return "Bottle";
+	}
+
+	@Override
+	public String description() {
+		return "A cool refreshing taste of blue raspberry ";
+	}
+
+	@Override
+	public String drink() {
+		return "Gulp....you suddenly feel the urge too play sports";
+	}
+
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return "Powerade";
+	}
+
+}
