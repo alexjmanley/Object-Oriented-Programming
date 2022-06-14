79f1ad47b286d849d0e44b0c3663ca951bb1c2c4
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-06-14 14:55:20 -0600

    Add files via upload
    Added Water class

---
+++ Final Project/Water.java
@@ -0,0 +1,35 @@
+package finalproject;
+
+public class Water implements Drink{
+
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return "Arrow Head";
+	}
+
+	@Override
+	public String color() {
+		// TODO Auto-generated method stub
+		return "Clear";
+	}
+
+	@Override
+	public String container() {
+		// TODO Auto-generated method stub
+		return "Bottle";
+	}
+
+	@Override
+	public String description() {
+		// TODO Auto-generated method stub
+		return "Clear quench thirsting water that will keep you hydrated all day!";
+	}
+
+	@Override
+	public String drink() {
+		// TODO Auto-generated method stub
+		return "sip......Amazing";
+	}
+
+}
