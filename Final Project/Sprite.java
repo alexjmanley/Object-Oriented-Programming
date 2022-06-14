1f7550d6f76cd4b8f35a97384050ac2a6cf6caad
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-06-14 14:56:54 -0600

    Add files via upload
    Added Sprite class

---
+++ Final Project/Sprite.java
@@ -0,0 +1,35 @@
+package finalproject;
+
+public class Sprite implements Drink{
+
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return "Sprite";
+	}
+
+	@Override
+	public String color() {
+		// TODO Auto-generated method stub
+		return "Clear";
+	}
+
+	@Override
+	public String container() {
+		// TODO Auto-generated method stub
+		return "Can";
+	}
+
+	@Override
+	public String description() {
+		// TODO Auto-generated method stub
+		return "A lemon fresh taste with zero caffiene";
+	}
+
+	@Override
+	public String drink() {
+		// TODO Auto-generated method stub
+		return "sip......You want to throw this away";
+	}
+
+}
