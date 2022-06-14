3f4ec213a581651dfdd97d1c3df2524a333b400d
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-06-14 14:50:09 -0600

    Delete VendingMachine.java

--- Final Project/VendingMachine.java
+++
@@ -1,6 +0,0 @@
-package finalprojectoo;
-
-public class VendingMachine {
-	// remember to change array to ArrayList so it can be be emptied
-	Drink[] options = {new Powerade(), new Powerade(), new Powerade()};
-}
