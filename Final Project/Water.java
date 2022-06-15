01fa8278b19c8f12eaeb257f995739e88bab137a
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-06-15 13:24:21 -0600

    Update Water.java
    updated Water class

--- Final Project/Water.java
+++ Final Project/Water.java
@@ -1,29 +1,11 @@
-package finalproject;
+package finalf;
 
 public class Water implements Drink{
 
 	@Override
-	public String name() {
-		// TODO Auto-generated method stub
-		return "Arrow Head";
-	}
-
-	@Override
-	public String color() {
-		// TODO Auto-generated method stub
-		return "Clear";
-	}
-
-	@Override
-	public String container() {
-		// TODO Auto-generated method stub
-		return "Bottle";
-	}
-
-	@Override
 	public String description() {
 		// TODO Auto-generated method stub
-		return "Clear quench thirsting water that will keep you hydrated all day!";
+		return "Clear quench thirsting water that will keep you hydrated all day! ";
 	}
 
 	@Override
