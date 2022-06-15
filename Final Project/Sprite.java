46d681a3eabc8f724288c5a4c95d1b6b9db3dd13
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-06-15 13:23:44 -0600

    Update Sprite.java
    updated Sprite class

--- Final Project/Sprite.java
+++ Final Project/Sprite.java
@@ -1,29 +1,11 @@
-package finalproject;
+package finalf;
 
 public class Sprite implements Drink{
 
 	@Override
-	public String name() {
-		// TODO Auto-generated method stub
-		return "Sprite";
-	}
-
-	@Override
-	public String color() {
-		// TODO Auto-generated method stub
-		return "Clear";
-	}
-
-	@Override
-	public String container() {
-		// TODO Auto-generated method stub
-		return "Can";
-	}
-
-	@Override
 	public String description() {
 		// TODO Auto-generated method stub
-		return "A lemon fresh taste with zero caffiene";
+		return "A lemon fresh taste with zero caffiene ";
 	}
 
 	@Override
