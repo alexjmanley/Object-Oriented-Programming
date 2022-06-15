c7afa7908ba45435c87078331ab5c8ce296f461d
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-06-15 13:22:07 -0600

    Update ExecuteFinalProject.java
    updated ExecuteFinalProject class

--- Final Project/ExecuteFinalProject.java
+++ Final Project/ExecuteFinalProject.java
@@ -1,4 +1,4 @@
-package finalproject;
+package finalf;
 
 import java.util.ArrayList;
 import java.util.Scanner; 
@@ -10,7 +10,7 @@ public class ExecuteFinalProject {
 		
 		ArrayList<Drink> coke = new ArrayList<>(); 
 		coke.add(new Coke()); coke.add(new Coke()); coke.add(new Coke());
-		ArrayList<Drink> dietCoke = new ArrayList<>();
+		ArrayList<Coke> dietCoke = new ArrayList<>();
 		dietCoke.add(new Coke()); dietCoke.add(new Coke()); dietCoke.add(new Coke()); 
 		ArrayList<Drink> sprite = new ArrayList<>(); 
 		sprite.add(new Sprite()); sprite.add(new Sprite()); sprite.add(new Sprite()); 
@@ -19,6 +19,8 @@ public class ExecuteFinalProject {
 		ArrayList<Drink> water = new ArrayList<>(); 
 		water.add(new Water()); water.add(new Water()); water.add(new Water()); 
 		
+		int i = 1; 
+		while (i == 1) {
 			System.out.println("Vending Machine");
 			System.out.println("1. Coke");
 			System.out.println("2. Diet Coke");
@@ -28,8 +30,32 @@ public class ExecuteFinalProject {
 			System.out.println(); 
 			System.out.println("Enter the number of the drink you want: ");
 			
-			
-			
+		try {
+			switch (input.nextInt()) {
+			case 1: System.out.println(coke.get(0).description() + coke.get(0).drink());
+					coke.remove(0);
+					break;
+			case 2: System.out.println(dietCoke.get(0).dietDescription() + dietCoke.get(0).drinkDiet());
+					dietCoke.remove(0);
+					break;
+			case 3: System.out.println(sprite.get(0).description() + sprite.get(0).drink());
+					sprite.remove(0);
+					break; 
+			case 4: System.out.println(powerade.get(0).description() + powerade.get(0).drink());
+					powerade.remove(0);
+					break; 
+			case 5: System.out.println(water.get(0).description() + water.get(0).drink());
+					water.remove(0);
+					break; 
+				}
+			}
+		catch (IndexOutOfBoundsException ex) {
+			System.out.println("Out of order!");
+			}
+		finally {
+			System.out.println();
+			}
+		}
 	}
 
 }
